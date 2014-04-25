0c646a3a1f740b192364f138e83619ab50ecb9c9
Brian Paff <bpaff@uci.edu>
2014-04-24 20:56:43 -0700

    Updates

--- Assignment4/Client.py
+++ Assignment4/Client.py
@@ -9,13 +9,25 @@ myname = raw_input('What is your name? ')
 class Client(Handler):
         
     def on_close(self):
-        pass
+        done = True
     
     def on_msg(self, msg):
-        print 'cli received %s' % str(msg)
-        self.close_when_done()
-        global done
-        done = True
+        temp = []
+        print msg
+        for m in msg.values():
+            temp.append(m)
+            print m
+        print "the first index is", temp[0]
+        if temp[0] == 'join':
+            name = temp[1]
+            print name, " has joined the room."
+        elif temp[0] == 'quit':
+            name = temp[1]
+            print name, " has left the room."
+        else:
+            name = temp[1]
+            print name, ":", temp[0]
+            
         
         
 host, port = 'localhost', 8888
@@ -31,7 +43,7 @@ def periodic_poll():
 thread = Thread(target=periodic_poll)
 thread.daemon = True # die when the main thread dies
 thread.start()
-
-while 1:
+done = False
+while done == False:
     mytxt = sys.stdin.readline().rstrip()
     client.do_send({'speak': myname, 'txt': mytxt})
--- Assignment4/Server.py
+++ Assignment4/Server.py
@@ -8,6 +8,7 @@ class Client:
     
     def __init__(self):
         pass
+    
 class MyHandler(Handler):
      
     def on_open(self):
@@ -17,15 +18,9 @@ class MyHandler(Handler):
         event_queue.append(('quit', self))
      
     def on_msg(self, msg):
-        i=0
-        temp = list()
-        lie = 0
-        for s in msg.values():
-            temp.append(s)
-        message = temp[0]
-        messages.append(message)
-        for m in messages:
+        for m in msg.values():
             print m
+        messages.append(msg)
  
  
 port = 8888
@@ -42,4 +37,4 @@ while 1:
     for h in handlers:
         for m in messages:
             h.do_send(m)
-            print "sent msg"
+    messages = []
